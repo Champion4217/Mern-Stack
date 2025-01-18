f631cc6cf6c7cd813a2e0e3794d447e977f8ca4f
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-01-18 13:08:51 +0530

    added

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,22 +1,113 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import { Box } from '@mui/joy'
+import React from "react";
+import Navbar from "../components/Navbar";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  Typography,
+} from "@mui/joy";
+import img1 from "../assests/contact.png";
 
 const Contact = () => {
-  return <>
-  <Navbar />
-  <Box sx={{
-    width:'100%',
-    height:'80vh'
-  }}>
-    <Box>
-       
-    </Box>
-    <Box>
+  return (
+    <>
+      <Navbar />
+      <Box
+        sx={{
+          width: "100%",
+          height: "80vh",
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              "@media (max-width: 1000px)": {
+              
+                width:'40%' // Hide image on small screens
+              },
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "3rem",
+              }}
+              component={"h1"}
+            >
+              Contact Us
+            </Typography>
+            <img  src={img1} alt="contact" />
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex", 
+                flexDirection: "column", 
+                gap: 2, 
+                maxWidth: "600px", 
+                margin: "auto",
+                width: {xs:'90%',sm:'90%' ,md:"80%", lg:'60%', xl:'50%'},
+              }}
+            >
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input placeholder="Enter Your Name" type="text" />
+              </FormControl>
 
-    </Box>
-  </Box>
-  </>
-}
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input placeholder="Enter Your Email" type="email" />
+              </FormControl>
 
-export default Contact
+              <FormControl>
+                <FormLabel>Message</FormLabel>
+
+                <Textarea
+                  name="message"
+                  placeholder="Type your message here..."
+                  required
+                  minRows={4}
+                  sx={{
+                    resize: "both",
+                    fontSize: "1rem",
+                    padding: "12px",
+                    borderRadius: "8px",
+                  }}
+                />
+              </FormControl>
+              <Button sx={{borderRadius:'20px'}}>
+                Submit
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Contact;
